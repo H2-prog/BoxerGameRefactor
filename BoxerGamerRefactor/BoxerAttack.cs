0c38d30d3c5f51d6c59427184c51159444064781
KingoBoiii <kingodk@outlook.dk>
2021-10-26 20:55:09 +0200

    Added foreground color to BoxerAttack

--- BoxerGamerRefactor/BoxerAttack.cs
+++ BoxerGamerRefactor/BoxerAttack.cs
@@ -6,13 +6,15 @@ namespace BoxerGamerRefactor
     {
         public string Title { get; set; }
         public ConsoleKey Key { get; set; }
+        public ConsoleColor ForegroundColor { get; set; }
         public int PlayerModifier { get; set; }
         public int ComputerModifier { get; set; }
 
-        public BoxerAttack(string title, ConsoleKey key, int playerModifier, int computerModifier)
+        public BoxerAttack(string title, ConsoleKey key, ConsoleColor color, int playerModifier, int computerModifier)
         {
             Title = title;
             Key = key;
+            ForegroundColor = color;
             PlayerModifier = playerModifier;
             ComputerModifier = computerModifier;
         }
